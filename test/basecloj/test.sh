e9663a09e4c4a66c159b48390c72a22f9bf5d875
Marc Andreu <marc@itqualab.com>
2024-05-24 14:20:02 +0900

    add test script for basecloj

---
+++ test/basecloj/test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd $(dirname "$0")
+source test-utils.sh
+
+# Template specific tests
+check "distro" lsb_release -c
+
+check "java_jdk" java --version
+check "clojure" clojure --version
+check "clj" clj --version
+check "user" id
+
+# Report result
+reportResults
