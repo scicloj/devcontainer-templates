371c85630fe50b2c05cd1f6a9833a7e878197030
Carsten Behring <carsten.behring@gmail.com>
2025-09-03 22:19:13 +0000

    fixed os_release

--- test/scicloj/test.sh
+++ test/scicloj/test.sh
@@ -3,7 +3,7 @@ cd $(dirname "$0")
 source test-utils.sh
 
 # Template specific tests
-check "distro" lsb_release -c
+check "distro" cat /etc/os-release
 check "clj" clj --version
 check "uv" uv --version
 check "R" R --version
