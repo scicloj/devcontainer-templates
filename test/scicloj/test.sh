af08a1cc411df0e6002e24bee7c46ce482fb84aa
Carsten Behring <carsten.behring@gmail.com>
2025-09-03 21:15:28 +0000

    more tests

--- test/scicloj/test.sh
+++ test/scicloj/test.sh
@@ -14,5 +14,6 @@ check "R" R --version
 check "lein" lein --version
 check "libpython-clj" clj -e '(require '\''[libpython-clj2.python :refer [py. py.. py.-] :as py])(py/initialize!)(py/run-simple-string "print(1+1)")'
 check "clojisr" clj -e '(require '\''[clojisr.v1.r :refer [r]])(r '\''(+ 1 2))(shutdown-agents)'
+check "pandas" uv sync && clj -e '(require '\''[libpython-clj2.require :refer [require-python]])(require-python '\''[pandas :as pd])'
 # Report result
 reportResults
