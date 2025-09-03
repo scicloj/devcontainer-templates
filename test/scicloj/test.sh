24fc3f5f8dbe60dbc9c88c07378c5882db705d38
Carsten Behring <carsten.behring@gmail.com>
2025-09-03 21:09:25 +0000

    fixed test

--- test/scicloj/test.sh
+++ test/scicloj/test.sh
@@ -9,12 +9,10 @@ check "distro" lsb_release -c
 #check "clojisr" clj -e "(require '[clojisr.v1.r :refer [r]])(r '(+ 1 2))"
 #check "pandas" clj -e "(require '[libpython-clj2.require :refer [require-python]])(require-python '[pandas :as pd])"
 check "clj" clj --version
-check "poetry" poetry --version
-check "bb" bb --version
+check "uv" uv --version
 check "R" R --version
-check "emacs" emacs --version
 check "lein" lein --version
-
+clj -e '(require '\''[libpython-clj2.python :refer [py. py.. py.-] :as py])(py/initialize!)(py/run-simple-string "print(1+1)")'
 
 # Report result
 reportResults
